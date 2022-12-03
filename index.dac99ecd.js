fe940fd739bff410a96a6bf12ed041a09831a430
Ynln69 <106525821+Ynln69@users.noreply.github.com>
2022-12-03 09:59:14 +0000

    Deploying to gh-pages from @ Ynln69/parcel-project-5@05553cf783ff8b10f1146db8ad9d8c62af3a7992 🚀

---
+++ index.dac99ecd.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("backdrop--hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.dac99ecd.js.map
