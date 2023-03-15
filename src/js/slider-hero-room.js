2d0764ca60b6afcc794a26ba085c17a587dec74e
Yana <yanalunina96@gmail.com>
2023-03-15 23:38:30 +0200

    створений слайдер на герої сторінки кімнати. потрібне доопрацювання

---
+++ src/js/slider-hero-room.js
@@ -0,0 +1,9 @@
+$('.room-time').slick({
+  // dots: true,
+  prevArrow: '<button type="button" class="my-slick-prev-room"></button>',
+  nextArrow: '<button type="button" class="my-slick-next-room"></button>',
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  adaptiveHeight: true,
+});
